a646fea61b98dde40cfbe353cffa80379c998c13
Vincent <xionghuih745@gmail.com>
2023-09-17 17:46:20 -0500

    init project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from 'react-dom';
+import item from './component/box.js';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export default class App extends React.Component{
+  render() {
+    return (
+      <div>
+        {item(1)}
+        {item(4)}
+        {item(7)}
+      </div>
+      )
+  }
+}
+
+ReactDom.render(<App/>, document.getElementById("root"));
